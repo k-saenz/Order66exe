bee74683426b6ed55192ad4b8461fd193092b027
BuildTools <unconfigured@null.spigotmc.org>
2021-11-25 16:23:25 -0600

    Removed most CSS to only necessary code

---
+++ Models/RuleModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order66exe.Models
+{
+    public class RuleModel
+    {
+        public int RuleId { get; set; }
+        //'true' for text channel; 'false' for voice channel
+        public bool Chat { get; set; }
+        public string Description { get; set; }
+
+        public RuleModel() { }
+
+        public RuleModel(bool chat, string desc)
+        {
+            Chat = chat;
+            Description = desc;
+        }
+
+    }
+}
