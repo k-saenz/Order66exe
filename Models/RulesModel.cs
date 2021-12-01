1836408f727f64aafafd3607a7b9f64c4ac0e0f7
BuildTools <unconfigured@null.spigotmc.org>
2021-11-30 21:21:40 -0600

    Migrated to .NET 5 with Account Auth

---
+++ Models/RulesModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order66exe.Models
+{
+    public class RulesModel
+    {
+        public int RuleId { get; set; }
+        //'true' for text channel; 'false' for voice channel
+        public bool Chat { get; set; }
+        public string Description { get; set; }
+
+        public RulesModel() { }
+
+        public RulesModel(bool chat, string desc)
+        {
+            Chat = chat;
+            Description = desc;
+        }
+    }
+}
